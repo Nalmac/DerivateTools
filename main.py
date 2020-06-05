6786cd3b76d74f37c4f039263ba5db0878412a47
Nalmac <simon.maciag@gmail.com>
2020-06-05 18:33:41 +0200

    Ajout de git ignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Derivator import Derivator
 
 def f(x):
-    return x**2
+    return x**3
 
 derivator = Derivator(f)
 
@@ -10,6 +10,4 @@ fPrime = derivator.returnDerivate()
 a = int(input("Borne de début >"))
 b = int(input("Borne de fin >"))
 
-derivator.findCommon(a, b)
-
-    
+derivator.findCommon(a, b)    
