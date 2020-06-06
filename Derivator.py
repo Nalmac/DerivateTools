bb062ea94a00084571b2388119aa0f148f917934
Nalmac <simon.maciag@gmail.com>
2020-06-06 11:33:04 +0200

    Ajout de commentaires utiles et constructifs

--- Derivator.py
+++ Derivator.py
@@ -1,21 +1,24 @@
 from sympy import Symbol, diff, lambdify
 import numpy
 
+#La classe qui fera tout le travail
 class Derivator():
     def __init__(self, fun, degr = 1):
         self.fun = fun
         self.var = Symbol('x')
         self.degr = degr
     
+    #Méthode de base qui dérive un fonction donnée dans le constructeur
     def derivate(self):
         return diff(self.fun(self.var), self.var, self.degr)
     
+    #Méthode qui convertit la dérivée obtenue au format sympy en une fonction utilisable
     def returnDerivate(self):
         self.prime = lambdify(self.var, self.derivate(), "numpy")
         return self.prime
     
+    #Méthode qui permet de savoir si, dans un intervalle donné, il existe un entier pour lequel la fonction et sa dérivée ont la même valeur
     def findCommon(self, a, b):
         for i in range(a, b):
             if self.fun(i) == self.prime(i):
                 print("Pour " + str(i) + ", la fonction et sa dérivée ont la même valeur.")
-
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from Derivator import Derivator
 
+#Fonction de test
 def f(x):
     return x**10
 
+#Construction de la classe Derivator, avec la fonction f comme fonction de travail
 derivator = Derivator(f)
 
+#On récupère la dérivée sous forme de lambda (fonction stockée dans une variable)
 fPrime = derivator.returnDerivate()
 
+#On demande les bornes de début et de fin à l'utilisateur
 a = int(input("Borne de début >"))
 b = int(input("Borne de fin >"))
 
+#On cherche s'il y a dans cet intervalle des nombres entiers tels que f(x) = f'(x)
 derivator.findCommon(a, b)
 
 #On peut dégager de ce programme que pour une fonction mononome de degré n, la fonction et sa dérivée ont la même valeur en deux points : 0 et n
