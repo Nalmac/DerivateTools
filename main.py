adfbbaa5879558ba293cb12afa476b632f4cfedd
Nalmac <simon.maciag@gmail.com>
2020-06-06 11:13:47 +0200

    Dégagement d'une propriété pour les fonctions de forme x**n

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Derivator import Derivator
 
 def f(x):
-    return x**3
+    return x**10
 
 derivator = Derivator(f)
 
@@ -10,4 +10,6 @@ fPrime = derivator.returnDerivate()
 a = int(input("Borne de début >"))
 b = int(input("Borne de fin >"))
 
-derivator.findCommon(a, b)    
+derivator.findCommon(a, b)
+
+#On peut dégager de ce programme que pour une fonction mononome de degré n, la fonction et sa dérivée ont la même valeur en deux points : 0 et n
