0f20096c3e274275e2306100e1c100210151e507
Nalmac <simon.maciag@gmail.com>
2020-06-08 09:38:29 +0200

    Ajout de la fonctionnalité permettant de trouver les égalités entre f et f' en interface graphique

--- UserInterface.py
+++ UserInterface.py
@@ -31,12 +31,15 @@ class Interface():
         
         self.FunButton = Button(self.Frame1, text="Dériver", command=self.funDerive)
 
+        self.Result = Label(self.Frame2, text="")
+
         self.Frame3 = LabelFrame(self.window, text="Quand est-ce que la fonction est égale à sa dérivée ?")
         self.a = StringVar()
-        self.BorneA = Entry(self.Frame3, textvariable=a).grid
+        self.BorneA = Entry(self.Frame3, textvariable=self.a)
         self.b = StringVar()
         self.BorneB = Entry(self.Frame3, textvariable=self.b)
         self.CalcuButton = Button(self.Frame3, text="Calculer", command=self.find)
+        self.CommonLabel = Label(self.Frame3, text="")
 
         self.start()    
 
@@ -56,13 +59,19 @@ class Interface():
         self.txtDerivate = self.derivator.derivate()
 
         #Affichage du résultat à l'utilisateur
-        string = "Dérivée : " + str(self.txtDerivate)
-        self.Result = Label(self.Frame2, text=string) 
-        self.Result.pack()
-    
+        self.Result['text'] = "Dérivée : " + str(self.txtDerivate)
+             
     def find(self):
-        a = 
-        points = self.derivator.findCommon()
+        a = int(self.a.get())
+        b = int(self.b.get())
+        points = self.derivator.findCommon(a, b)
+    
+        string = "La fonction et sa dérivée ont la même valeur pour les valeurs de x suivantes : "
+
+        for point in points:
+            string += str(point) + " "
+        
+        self.CommonLabel['text'] = string
 
     def start(self):
         self.Frame1.pack(fill=X, side=TOP, padx=10)
@@ -74,7 +83,13 @@ class Interface():
         self.x.pack(side=LEFT)
         self.InputD.pack(side=LEFT)
         self.FunButton.pack(side=RIGHT, padx=2, fill=Y)
+        self.Result.pack(pady=10)
         self.Frame2.pack()
+        self.Frame3.pack()
+        self.BorneA.pack()
+        self.BorneB.pack()
+        self.CalcuButton.pack()
+        self.CommonLabel.pack()
         self.window.mainloop()
 
 #Debug Part
